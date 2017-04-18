530a9fa90e7a808e2b56aa69a69fa27da7f65c4a
PhilipBoyle <phlpbyl@gmail.com>
2017-04-18 11:48:28 +0100

    Created Main Menu Script

--- Assets/Scripts/MainMenuBehaviour.cs
+++ Assets/Scripts/MainMenuBehaviour.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MainMenuBehaviour : MonoBehaviour {
+    public void TriggerMenuBehaviour (int i)
+    {
+        switch (i)
+        {
+            default:
+            case (0):
+                SceneManager.LoadScene("Game");
+                break;
+            case (1):
+                Application.Quit();
+                break;
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
